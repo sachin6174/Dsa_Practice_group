f41d3cbd0a8e5c9e4c2db8465d9b7f28f53ab963
bunty chauhan <buntychauhan1464@gmail.com>
2023-03-03 23:29:41 +0530

    patterns

---
+++ bunty/7_patter.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+   int n;
+   cin>>n;
+
+   int size=2n;
+   int arr[2n];
+   for(int i=0;i<2n;i++){
+     arr[i]=i+1;
+   }
+
+    return 0;
+}
