448fcf78f6f57f14eaa31f068d2f789951fbac50
sachin6174 <sachinmehtab@gmail.com>
2023-03-07 22:41:22 +0530

    seperation of seq

---
+++ sachin/9_bargraph.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[]={4,5,0,8,3,9,1,3,4,5,6,0,9};
+    int n=sizeof(a)/sizeof(int);
+
+    int maxi=a[0];
+    for(int i=0;i<n;i++){
+        if(a[i]>maxi){
+            maxi=a[i];
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+
+        cout<<endl;
+    }
+
+  
+}
