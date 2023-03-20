340cb23a45ac313a1e96b4ed7d05288c7790d18a
sachin6174 <sachinmehtab@gmail.com>
2023-03-20 15:55:48 +0530

    bunty_branch

--- Alok/Java/src/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-        // System.out.println("count " + count);
-        if(count >= 2){
-            System.out.println("false");
-        }else{
-            System.out.println("true");
-        }
-        }
-}
