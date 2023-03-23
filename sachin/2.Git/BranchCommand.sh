f804184155cb04b9815396532dce1e449164099f
sachin6174 <sachinmehtab@gmail.com>
2023-03-23 22:34:36 +0530

    git commands

---
+++ sachin/2.Git/BranchCommand.sh
@@ -0,0 +1,10 @@
+git branch -a  (it will list all branch as well as put green color to the branch u are in)
+
+git branch new-branch-name from-which-branch
+(
+    git branch branch-name    -> will make branch from master
+)
+
+git checkout branch-name    (to change the branch)
+
+git branch -d branch-name
