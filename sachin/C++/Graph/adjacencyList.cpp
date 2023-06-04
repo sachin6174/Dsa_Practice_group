a187c52d52d6b5644d0c83f4d90f563b1f6f60e7
Sachin kumar <sachinmehtab@gmail.com>
2023-06-04 14:41:52 +0530

    changes

---
+++ sachin/C++/Graph/adjacencyList.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class adjacencyList{
+    public:
+        int vertex;
+        vector<int> neighbour;
+};
+
+int main(){
+    int n;
+    cout<<"sachin"<<endl;
+    return 0;
+}
